7e4e1ebcf8c648eef237bbcee53976b9d6e2ac63
Pushpraj Ruhal <emai2pushpraj@ruhal.in>
2019-03-22 12:48:50 +0800

    #318280: Date field breaks with SXA + Context language with non Gregorian calendar (e.g. UmAlQuraCalendar in ar-SA)

---
+++ src/Sitecore.Support.318280/Forms.Mvc.ViewModels.Fields/DateField.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace Sitecore.Support.Forms.Mvc.ViewModels.Fields
+{
+  public class DateField: Sitecore.Forms.Mvc.ViewModels.Fields.DateField
+  {
+    public override void Initialize()
+    {
+      CultureInfo currentCulture = Thread.CurrentThread.CurrentCulture;
+      Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+      base.Initialize();
+
+      Thread.CurrentThread.CurrentCulture = currentCulture;
+
+    }
+  }
+}
---
+++ src/Sitecore.Support.318280/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.318280")]
+[assembly: AssemblyProduct("Sitecore.Support.318280")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Date field breaks with SXA + Context language with non Gregorian calendar (e.g. UmAlQuraCalendar in ar-SA)")]
+
