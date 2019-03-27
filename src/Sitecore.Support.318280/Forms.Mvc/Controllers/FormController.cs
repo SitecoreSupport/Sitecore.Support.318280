62f3f963d44ac349316a2a6f59ea7a42f6102ccf
Pushpraj Ruhal <emai2pushpraj@ruhal.in>
2019-03-27 17:26:36 +0800

    #318280: added fix to cover submit scenario

---
+++ src/Sitecore.Support.318280/Forms.Mvc/Controllers/FormController.cs
@@ -0,0 +1,28 @@
+﻿using Sitecore.Forms.Mvc.Attributes;
+using Sitecore.Forms.Mvc.Controllers.Filters;
+using Sitecore.Forms.Mvc.Controllers.ModelBinders;
+using Sitecore.Forms.Mvc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore.Support.Forms.Mvc.Controllers
+{
+  public class FormController: Sitecore.Forms.Mvc.Controllers.FormController
+  {
+    public override ActionResult Index([ModelBinder(typeof(FormModelBinder))] FormViewModel formViewModel)
+    {
+      CultureInfo currentCulture = Thread.CurrentThread.CurrentCulture;
+      Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+      var result = base.Index(formViewModel);
+
+      Thread.CurrentThread.CurrentCulture = currentCulture;
+      return result;
+    }
+  }
+}
---
+++ src/Sitecore.Support.318280/Forms.Mvc/Routes/InitializeRoutes.cs
@@ -0,0 +1,33 @@
+﻿using Sitecore.Forms.Mvc;
+using Sitecore.Pipelines;
+using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Sitecore.Support.Forms.Mvc.Routes
+{
+  public class InitializeRoutes
+  {
+    public virtual void Process(PipelineArgs args)
+    {
+      InitializeRoutes.RegisterClientEventController();
+    }
+
+    private static void RegisterClientEventController()
+    {
+      RouteCollectionExtensions.MapRoute(RouteTable.Routes, Sitecore.Forms.Mvc.Constants.Routes.ClientEvent, "clientevent/{action}", new
+      {
+        controller = "ClientEvent",
+        action = "Process"
+      });
+      RouteCollectionExtensions.MapRoute(RouteTable.Routes, Sitecore.Forms.Mvc.Constants.Routes.Form, "form/{action}", new
+      {
+        controller = "Form",
+        action = "Process"
+      }, new string[]
+      {
+                "Sitecore.Support.Forms.Mvc.Controllers"
+      });
+    }
+  }
+}
